c33089287f5c89a899f6d46ba9c56d7740bf717d
Tillmann Heigel <heigel@tillmannheigel.de>
2018-02-16 09:44:29 +0100

    inital commit with maven archetype:generate

---
+++ src/main/java/de/tillmannheigel/annotations/App.java
@@ -0,0 +1,18 @@
+package de.tillmannheigel.annotations;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+    private void testMethod(@RequestEnumParam String string){
+
+    }
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
